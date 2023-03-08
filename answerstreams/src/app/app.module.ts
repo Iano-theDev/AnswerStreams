64f80a585420979b189fa0d0ce5e1a69e0b4b0c1
Iano-theDev <sinceianmike@gmail.com>
2023-03-08 15:20:36 +0300

    Update colors

--- answerstreams/src/app/app.module.ts
+++ answerstreams/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./shared/components/header/header.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @NgModule({
     declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from "./shared/components/header/header.component";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ]
 })
 export class AppModule { }
