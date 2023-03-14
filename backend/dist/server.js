0be1b2755afce43f5f2c0c9f5fc94c6b14a259ef
Iano-theDev <sinceianmike@gmail.com>
2023-03-14 17:53:32 +0300

    initialize backend

---
+++ backend/dist/server.js
@@ -0,0 +1,5 @@
+"use strict";
+const a = 2;
+const b = 5;
+const add = () => (a + b);
+console.log(add());
---
+++ backend/src/server.ts
@@ -0,0 +1,6 @@
+const a:number = 2;
+const b:number = 5;
+
+const add = ()=>( a + b)
+
+console.log(add())
